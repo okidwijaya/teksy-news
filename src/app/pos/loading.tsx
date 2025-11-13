e891b0efe8763a3111e42a14a420dd39447660b9
okidwijaya <okidwijaya@gmail.com>
2025-11-13 16:24:10 +0700

    fix: err vercel

--- src/app/pos/loading.tsx
+++ src/app/pos/loading.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react'
 import Image from 'next/image'
-import logo from "../../public/final-logo.svg";
+import logo from "../../../public/final-logo.svg";
 
 export default function LoadingLogoDefault() {
     return (
